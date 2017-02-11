f4447000fb87a1d0c7f2110c5dbc58dbd44b907d
Matt-Hurd <sabahbob@gmail.com>
2017-02-11 15:55:20 -0800

    Wakeup message

---
+++ startup.py
@@ -0,0 +1,9 @@
+import socket
+
+UDP_IP = "10.138.144.76"
+UDP_PORT = 5005
+MESSAGE = "WHISPER"
+
+sock = socket.socket(socket.AF_INET,
+                     socket.SOCK_DGRAM)
+sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
