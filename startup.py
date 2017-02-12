5f42bbbec974f08bd8cd0af2a1a387009cf8a928
Matt-Hurd <sabahbob@gmail.com>
2017-02-11 17:47:51 -0800

    Change port

--- startup.py
+++ startup.py
@@ -10,7 +10,7 @@ sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 
 
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+UDP_PORT = 5010
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind(('', UDP_PORT))
