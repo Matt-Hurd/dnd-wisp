c69371ed797f143d663b093af03c81d52a176fcb
all-hack <obelanger@fordham.edu>
2017-02-12 01:13:36 -0800

    almosssssst

--- den_spider.py
+++ den_spider.py
@@ -64,20 +64,6 @@ import psycopg2
 
 
 
-# insert = "INSERT INTO urls \
-# (url, domain, path, scraped, created_at, updated_at) \
-# VALUES ('{url_val}', '{domain_val}', '{path_val}', {scraped_val}, now(), now())\
-# ON CONFLICT (url) DO NOTHING;\
-# ".format(
-#     url_val=rows[0][1],
-#     domain_val=rows[0][2],
-#     path_val=rows[0][3],
-#     scraped_val=rows[0][4],
-# )
-
-# print (cur.execute(insert))
-
-# conn.commit()
 
 
 
@@ -135,170 +121,53 @@ link_output = []
 #     scraped_val=rows[0][4],
 
 
+url_insert = ""
+url_columns = "(url, domain, path, scraped, created_at, updated_at)"
+found_urls = set()
 for i, link in enumerate(links):
     parsed = urlparse(link)    
     if parsed.netloc:
         url = link.split('?')[0].split('#')[0]
-        link_output.append({"url":url, "domain":parsed.netloc, "path":parsed.path, "scraped":'0'})
+        if not (url in found_urls):
+            found_urls.add(url)        
+            url_insert += "('{url_val}', '{domain_val}', '{path_val}', {scraped_val}, now(), now())".format(
+                    url_val=url,
+                    domain_val=parsed.netloc,
+                    path_val=parsed.path,
+                    scraped_val=False,                
+                )
 
 
 
-
-found_frameworks = set()
-frameworks = {
-    '//link/@href': {
-        'bootstrap': 'bootstrap',
-        'font_awesome': 'font-awesome',
-        'google_font_api': 'fonts.google',
-        'ionicons': 'ionicons',
-        'lightbox': 'lightbox',
-        'materialize_css': 'materialize',
-        'outlook_web_app': '/themes/resources/owafont',
-        'rdoc': 'rdoc-style.css',
-        'wordpress': 'wp-'
-    },
-    "//input/@name": {
-        'stripe': 'fullstripe',
-        'django': 'csrfmiddlewaretoken',
-        'infusionsoft': 'infusionsoft_version',
-        'microsoft_asp_net': 'VIEWSTATE',
-        'oracle_commerce': '_dyncharset'
-    },
-    "//a/@href": {
-        'bugzilla': 'enter_bug.cgi'
-    },
-    "//script": {
-        'buy_sell_ads': 'bsa.src',
-        'segment': 'segment.com/analytics',
-        'spree': 'Spree.'
-    },
-    "//input/@value": {
-        'paypal': '_s-xclick'
-    },
-    "//iframe/@src": {
-        'tumblr': 'tumblr.com'
-    },
-    "//a": {
-        'reddit': 'Powered by Reddit'
-    }
-}
+# for i, link in link_output:
+#     print ()
+#     print (link)
+#     insert_link
 
 
-for framework_type, items in frameworks.items():
-    inputs = tree.xpath(framework_type)
-    for i in inputs:
-        if not '@' in framework_type:
-            i = i.text
-            if not i:
-                continue
-        for key, value in items.items():
-            if value in i:
-                found_frameworks.add(key)
-
-
-
-
-names = [
-    'keywords',
-    'description',
-    'author',
-    'twitter_site',
-    'twitter_title',
-    'twitter_description',
-    'twitter_image',
-    'twitter_creator',
-]
-
-properties = [
-    'og_title',
-    'og_url',
-    'og_description',
-    'og_image',
-    'og_site_name',
-]
-
-found_metas = {}
-metas = root.xpath("//head/meta") 
-for meta in metas:
-    if 'name' in meta.attrib.keys():
-        if meta.attrib['name'] in names:
-            found_metas[meta.attrib['name']] = meta.attrib['content']
-    if 'property' in meta.attrib.keys():
-        if meta.attrib['property'] in properties:
-            found_metas[meta.attrib['property']] = meta.attrib['content']
 
 
 
 print ()
-print ("links")
-print (link_output)
-
-print ()
-print ("found_metas")
-print (found_metas)
-
-print ()
-print ("found_frameworks")
-print (found_frameworks)
-
-
-lanks = ""
-if (link_output):
-
-    for i, lank in enumerate(link_output):
-        if (i == 0):
-            lanks = "{url}='{u_v}'".format(key=meta, value=found_metas[meta])
-        else:
-            lanks += " and {key}='{value}'".format(key=meta, value=found_metas[meta])
+print (url_columns)
+print (url_insert)
 
 
 
+# insert = "INSERT INTO urls \
+# (url, domain, path, scraped, created_at, updated_at) \
+# VALUES ('{url_val}', '{domain_val}', '{path_val}', {scraped_val}, now(), now())\
+# ON CONFLICT (url) DO NOTHING;\
+# ".format(
+#     url_val=rows[0][1],
+#     domain_val=rows[0][2],
+#     path_val=rows[0][3],
+#     scraped_val=rows[0][4],
+# )
 
-metas = ""
-if (found_metas):    
+# print (cur.execute(insert))
 
-    for i, meta in enumerate(found_metas):
-        if (i == 0):
-            metas = "{key}='{value}'".format(key=meta, value=found_metas[meta])
-        else:
-            metas += " and {key}='{value}'".format(key=meta, value=found_metas[meta])
-        
-
-
-frameworks = ""
-if (found_frameworks):    
-
-    for i, framework in enumerate(found_frameworks):
-        if (i == 0):
-            frameworks = "{frame}='1'".format(frame=framework)
-        else:
-            frameworks += " and {frame}='1'".format(frame=framework)
-
-print ()
-print ()
-
-print ("links")
-
-
-print ("metas")
-print (metas)
-
-print ("frameworks")
-print (frameworks)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# conn.commit()
 
 
 
