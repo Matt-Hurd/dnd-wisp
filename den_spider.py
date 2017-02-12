8ac123d0665b68ac8144ec30fdfb25171f8ffe78
all-hack <obelanger@fordham.edu>
2017-02-11 17:49:21 -0800

    links are grabbed, working on filtering

--- den_spider.py
+++ den_spider.py
@@ -16,7 +16,7 @@ tree = etree.parse(StringIO(r.text), parser)
 
 root = tree.getroot()
 
-links = tree.xpath("//a")
+links = tree.xpath("//@href")
 head = root.xpath("//head")
 metas = root.xpath("//head/meta") 
 
@@ -32,14 +32,27 @@ print (links)
 print (head)
 print (metas)
 
-print (dir(metas[0]))
+print ()
+
+print (dir(links[0]))
 
 
 
-for index, elm in enumerate(metas):
-    print ("{index}: tag: {a}, text: {b}".format(index=index, a=elm.tag, b=elm.text))
-    for i, keys in enumerate(elm.keys()):
-        print ("\t{i}: {c} = '{d}'".format(i=i, c=keys, d=elm.values()[i]))
+# print (links[0].find())
+
+# for index, link in enumerate(links):
+#     print ("{index}: tag: {a}, text: {b}".format(index=index, a=link.tag, b=link.text))
+
+#     for i, keys in enumerate(link.keys()):
+#         print ("\t{i}: {c} = '{d}'".format(i=i, c=keys, d=link.values()[i]))
+
+
+
+
+# for index, elm in enumerate(metas):
+    # print ("{index}: tag: {a}, text: {b}".format(index=index, a=elm.tag, b=elm.text))
+#     for i, keys in enumerate(elm.keys()):
+#         print ("\t{i}: {c} = '{d}'".format(i=i, c=keys, d=elm.values()[i]))
     
     
 
