513d4a1629bcdc2d07ac1d2b94dc4bd96dc508a4
Matt-Hurd <sabahbob@gmail.com>
2017-02-12 05:32:03 -0800

    oh god

--- startup.py
+++ startup.py
@@ -15,6 +15,10 @@ UDP_PORT = 5010
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind(('', UDP_PORT))
+
+for x in range(16):
+    os.spawnl(os.P_DETACH, 'python3 /root/dnd-wisp/den_spider.py')
+
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     if addr[0] == '138.197.194.84' or addr[0] == '10.138.144.76':
